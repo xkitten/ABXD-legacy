5dc0935af5f4b12cad5c7fb2f430c166f2ff20f1
GlitchMr <glitchmr@myopera.com>
2012-06-09 16:04:11 +0200

    ABXD 2 - the beginning

---
+++ css/themelist.php
@@ -0,0 +1,39 @@
+<?php
+//  AcmlmBoard XD - Theme list
+
+//This file lists all the possible choices for user themes.
+//2.2.3 UPDATE: In the next version, 3.0, this file will no longer be used,
+//in favor of an easier system.
+
+$themes = array(
+	"default"=>"TRaSH (Default)",
+
+	//Packins
+	"abxd20"=>"ABXD 2.0",
+	"defaultold"=>"ABXD Classic (rounded)",
+	"defaultsharp"=>"ABXD Classic (sharp)",
+	"cycle"=>"Daily Cycle",
+	"dos"=>"DOS-like",
+	"grayscale"=>"Grayscale",
+	"monogreen"=>"Mono Green",
+	"cheese"=>"Old Cheese",
+
+	//Additions
+);
+
+$themeBylines = array(
+	"default"=>"Based on GarBG by FirePhoenix<br />Messed up by Kawa",
+
+	"abxd20"=>"Design by Kawa",
+	"cheese"=>"Original by Acmlm<br />Ported by Kawa",
+	"cycle"=>"Design by Kawa",
+	"defaultold"=>"Design by Kawa",
+	"defaultsharp"=>"Design by Kawa",
+	"dos"=>"Design by Kawa",
+	"grayscale"=>"Design by Kawa<br />Variant on ABXD Classic.",
+	"monogreen"=>"Design by Kawa",
+
+);
+
+
+?>
---
+++ lib/settings.php
@@ -0,0 +1,45 @@
+<?php
+//Generated and parsed by the Board Settings admin panel.
+
+//Settings
+$boardname = "ABXD";
+$logoalt = "Another ABXD Board";
+$logotitle = "Another ABXD Board";
+$dateformat = "m-d-y, h:i A";
+$autoLockMonths = 4;
+$warnMonths = 0;
+$customTitleThreshold = 100;
+$viewcountInterval = 10000;
+$overallTidy = 0;
+$noAjax = 0;
+$noGuestLayouts = 1;
+$theWord = "";
+$systemUser = 0;
+$minWords = 2;
+$minSeconds = 30;
+$uploaderCap = 32;
+$personalCap = 0.25;
+$uploaderMaxFileSize = 2;
+$uploaderWhitelist = "png gif jpg jpeg txt css zip rar 7z ogg mp3 swf ogv mp4 webm";
+$mailResetFrom = "noreply@example.net";
+$lastPostsTimeLimit = 72;
+
+//Hacks
+$hacks['forcetheme'] = "";
+$hacks['themenames'] = 0;
+
+//Profile Preview Post
+$profilePreviewText = "This is a sample post. It demonstrates various kinds of [i]markup[/i] and quotes and such.
+This has to be one of the [b]longest[/b] preview messages ever seen in an AcmlmBoard.[quote=Barack Obama]I am Barack Obama and I approve this preview message.[quote=some guy]This is a nested quote. See what you get.[/quote][/quote]Well, what more could you [url=http://en.wikipedia.org]want to know[/url]? Perhaps how to do the classic infinite loop?
+[source=qbasic]10 PRINT \"Hello World!\"
+20 GOTO 10[/source]HEEEEY MACARONI! :D";
+
+//Meta
+$metaDescription = "The owner of this ABXD board was too lazy to edit the description field. Kill him.";
+$metaKeywords = "abxd,acmlmboard";
+
+//RSS
+$feedname = "New ABXD Feed";
+$rssblurb = "The latest replies on the board.";
+
+?>
